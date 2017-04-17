a213ca0304bdbd1dbd08795cd85fd31a3f393226
Sebastian Thoss <dj_thossi@web.de>
2017-04-17 16:05:10 +0200

    clean up image folder for blog

---
+++ _scripts/findUnusedImages.php
@@ -0,0 +1,33 @@
+<?php
+
+function readFiles($directory) {
+    $excludedFiles = ['.', '..', '.gitkeep'];
+
+    $files = [];
+    foreach ( scandir($directory) as $file) {
+        if (in_array($file, $excludedFiles) || is_dir($directory . $file)) {
+            continue;
+        }
+
+        $files[] = $file;
+    }
+
+    return $files;
+}
+
+$imagePath = __DIR__ . "/../source/assets/img/blog/";
+
+$images = readFiles($imagePath);
+
+$blogPath = __DIR__ . "/../source/_reiseblog/";
+
+foreach(readFiles($blogPath) as $entry) {
+	$content = file_get_contents($blogPath . $entry);
+	foreach($images as $key => $image) {
+		if (strpos($content, $image) !== false) {
+			unset($images[$key]);
+		}
+	}
+}
+
+var_dump($images);
