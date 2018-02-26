439688ed1d6dd6e960fecb5c51808fc4afeda317
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2018-02-26 09:35:34 +0800

    add first images to familie fotos page

--- _scripts/ImageGenerator/data/existingImages.php
+++ _scripts/ImageGenerator/data/existingImages.php
@@ -1,2 +1,8 @@
 <?php $existingImages = array (
+  'IMG_4172.jpg' => 'IMG_4172.jpg',
+  'IMG_4172_01.jpg' => 'IMG_4172_01.jpg',
+  'IMG_4186.jpg' => 'IMG_4186.jpg',
+  'IMG_4201.jpg' => 'IMG_4201.jpg',
+  'IMG_4208.jpg' => 'IMG_4208.jpg',
+  'IMG_4208_01.jpg' => 'IMG_4208_01.jpg',
 );
