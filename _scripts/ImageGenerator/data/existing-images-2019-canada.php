738df70aa3b2eb9b6c8e65c8c757f6dc096b410b
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2019-03-20 07:19:40 +0100

    Canada Brothers day 5

--- _scripts/ImageGenerator/data/existing-images-2019-canada.php
+++ _scripts/ImageGenerator/data/existing-images-2019-canada.php
@@ -4,4 +4,6 @@
   'IMG_7811.jpg' => 'IMG_7811.jpg',
   'LRG_MTS00552.jpg' => 'LRG_MTS00552.jpg',
   'IMG_5264.jpg' => 'IMG_5264.jpg',
+  'IMG_7840.jpg' => 'IMG_7840.jpg',
+  'IMG_7843.jpg' => 'IMG_7843.jpg',
 );
