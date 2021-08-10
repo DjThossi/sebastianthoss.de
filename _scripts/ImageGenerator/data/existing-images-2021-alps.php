bf760679122c724a9b835aa046360674e73dee19
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2021-08-10 11:11:48 +0200

    Urlaub 2021 in Familie

--- _scripts/ImageGenerator/data/existing-images-2021-alps.php
+++ _scripts/ImageGenerator/data/existing-images-2021-alps.php
@@ -1,3 +1,6 @@
 <?php $existingImages = array (
   'IMG_0435.jpg' => 'IMG_0435.jpg',
+  'IMG_0543.jpg' => 'IMG_0543.jpg',
+  'IMG_0545.jpg' => 'IMG_0545.jpg',
+  'IMG_0548.jpg' => 'IMG_0548.jpg',
 );
