175af4928ea6c234286b9929e104e994eb71f175
Sebastian Thoss <thoss_sebastian@web.de>
2026-01-11 20:28:04 +0800

    25.12.25 Tradition trifft Moderne

--- _scripts/ImageGenerator/data/existing-images-2025-vietnam-bali.php
+++ _scripts/ImageGenerator/data/existing-images-2025-vietnam-bali.php
@@ -1,4 +1,5 @@
 <?php $existingImages = array (
-  'IMG_1283.jpg' => 'IMG_1283.jpg',
   'IMG_5393.jpg' => 'IMG_5393.jpg',
+  'IMG_1283.jpg' => 'IMG_1283.jpg',
+  'IMG_1310.jpg' => 'IMG_1310.jpg',
 );
