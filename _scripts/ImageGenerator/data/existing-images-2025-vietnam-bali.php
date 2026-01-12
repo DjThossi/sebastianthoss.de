62980eab2cf90eb26ca0f2434273bbeb0e8c6541
Sebastian Thoss <thoss_sebastian@web.de>
2026-01-12 11:43:49 +0800

    26.12.25 Auf nach Ho-Chi-Minh

--- _scripts/ImageGenerator/data/existing-images-2025-vietnam-bali.php
+++ _scripts/ImageGenerator/data/existing-images-2025-vietnam-bali.php
@@ -2,4 +2,6 @@
   'IMG_5393.jpg' => 'IMG_5393.jpg',
   'IMG_1283.jpg' => 'IMG_1283.jpg',
   'IMG_1310.jpg' => 'IMG_1310.jpg',
+  'IMG_1368.jpg' => 'IMG_1368.jpg',
+  'IMG_5563.jpg' => 'IMG_5563.jpg',
 );
