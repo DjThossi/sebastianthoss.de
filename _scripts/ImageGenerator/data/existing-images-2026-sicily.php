5781fbe7f1e061ac2e09c1c816332f3ed542b9ad
Sebastian Thoss <thoss_sebastian@web.de>
2026-06-21 20:30:13 +0200

    Add new Sicily blog posts for 29.04.2026 and 01.05.2026 with updated assets

--- _scripts/ImageGenerator/data/existing-images-2026-sicily.php
+++ _scripts/ImageGenerator/data/existing-images-2026-sicily.php
@@ -1,3 +1,6 @@
 <?php $existingImages = array (
+  'IMG_7047.jpg' => 'IMG_7047.jpg',
+  'IMG_7051.jpg' => 'IMG_7051.jpg',
+  'IMG_3304.jpg' => 'IMG_3304.jpg',
   'IMG_7212.jpg' => 'IMG_7212.jpg',
 );
