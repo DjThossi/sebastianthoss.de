8e00e5f85c7a4292e36809bf2fabe3ebf217876a
Sebastian Thoss <thoss_sebastian@web.de>
2026-06-28 11:27:22 +0200

    Die ersten 4 Posts

--- _scripts/ImageGenerator/data/existing-images-2026-sicily.php
+++ _scripts/ImageGenerator/data/existing-images-2026-sicily.php
@@ -3,4 +3,6 @@
   'IMG_7051.jpg' => 'IMG_7051.jpg',
   'IMG_3304.jpg' => 'IMG_3304.jpg',
   'IMG_7212.jpg' => 'IMG_7212.jpg',
+  'IMG_3338.jpg' => 'IMG_3338.jpg',
+  'DSC02174.jpg' => 'DSC02174.jpg',
 );
