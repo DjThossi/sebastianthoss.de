8c9f12caded1a26fe916d76694332e028af64ab6
Sebastian Thoss <thoss_sebastian@web.de>
2026-06-30 13:41:52 +0200

    Sizilien

--- _scripts/ImageGenerator/data/existing-images-2026-sicily.php
+++ _scripts/ImageGenerator/data/existing-images-2026-sicily.php
@@ -2,7 +2,6 @@
   'IMG_7047.jpg' => 'IMG_7047.jpg',
   'IMG_7051.jpg' => 'IMG_7051.jpg',
   'IMG_3304.jpg' => 'IMG_3304.jpg',
-  'IMG_7212.jpg' => 'IMG_7212.jpg',
   'IMG_3338.jpg' => 'IMG_3338.jpg',
   'DSC02174.jpg' => 'DSC02174.jpg',
   'IMG_7111.jpg' => 'IMG_7111.jpg',
@@ -10,4 +9,6 @@
   'DSC02271.jpg' => 'DSC02271.jpg',
   'IMG_3712.jpg' => 'IMG_3712.jpg',
   'IMG_3922.jpg' => 'IMG_3922.jpg',
+  'IMG_7205.jpg' => 'IMG_7205.jpg',
+  'IMG_7212.jpg' => 'IMG_7212.jpg',
 );
